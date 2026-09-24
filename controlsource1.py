3d34855cb22fbd6962b394460d2452bd9b5ec73e
mimyutt <mutiakhofifaturr@gmail.com>
2026-09-24 10:56:38 +0700

    first commit

---
+++ controlsource1.py
@@ -0,0 +1,12 @@
+presentase = float(input("Masukkan presentase : "))
+
+if presentase >= 90:
+    print("Performa sangat baik")
+elif presentase >= 80:
+    print("Performa sangat bagus")
+elif presentase >= 70:
+    print("Performa bagus")
+elif presentase >= 60:
+    print("Performa rata-rata")
+else :
+    print("Performa kurang")
